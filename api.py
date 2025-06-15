62996bb2a2025641bfd3ad71a14ac00642ea00c7
David Atobatele <davidatobatele2@gmail.com>
2025-06-15 20:38:09 +0100

    Update api.py

--- api.py
+++ api.py
@@ -39,4 +39,4 @@ def serve_demo():
     return send_from_directory("static", "index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=true)
+    app.run(host="0.0.0.0", port=5000, debug=True)
